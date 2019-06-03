8c953b6221d46e180af17117f79c06b3853aa262
Carlos Marinho <carluizfla@hotmail.com>
2019-06-03 17:03:24 -0300

    added Search Bar

--- src/main.js
+++ src/main.js
@@ -1,1 +1,19 @@
 import Vue from 'vue';
+import App from './App';
+
+//Here is another way to use the el and more used
+new Vue({
+    render: h => h(App)
+}).$mount('#app');
+
+/*
+new Vue({
+    el: '#app',
+    render: h => h(App)
+})*/
+
+/*new Vue({
+    render: function(createElement) {
+        return createElement(App);
+    }
+})*/
