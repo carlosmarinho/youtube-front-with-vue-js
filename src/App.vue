d6e6742307e02b0acd50178003b66cb87dbb3dbf
Carlos Marinho <carluizfla@hotmail.com>
2019-06-18 08:49:51 -0300

    alteraçoes

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,36 @@
 <template>
     <div>
         <SearchBar @termChange="onTermChange"></SearchBar>
+        <VideoList v-bind:videos="videos"></VideoList>
+        <!--the first videos here is the name of the props
+           and the second one is the data.videos 
+           in react this second one will be the state -->
     </div>
 </template>
 
+
 <script>
+/* working with vue instance 
+new Vue({
+    el: '#app',
+    data: {//Initialize our list of 'ingridients
+        textInput: ''
+    }
+})*/
+
 import axios from 'axios';
 import SearchBar from './components/SearchBar';
+import VideoList from './components/VideoList';
 const API_KEY = 'AIzaSyB6tTonrxmyofeQ0rrbwbms718G7pi8Mzs';
 
 export default{
     name: 'App',
     components: {
-        SearchBar
+        SearchBar,
+        VideoList
+    },
+    data() {
+        return { videos: [] }
     },
     methods: {
         onTermChange: function(searchTerm) {
@@ -25,7 +43,9 @@ export default{
                         q: searchTerm
                     }
                 })
-                .then(response => console.log(response));
+                .then(response => {
+                    this.videos = response.data.items;
+                });
         }
     }
 }
