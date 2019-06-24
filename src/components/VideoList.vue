40f730c4cbcd3129f0857afa4714a8b7c0189140
Carlos Marinho <carluizfla@hotmail.com>
2019-06-24 16:58:56 -0300

    finalizado o app

--- src/components/VideoList.vue
+++ src/components/VideoList.vue
@@ -1,7 +1,12 @@
 <template>
-    <ul>
-        VideoList
-        {{ videos.length }}
+    <ul class="list-group col-md-4">
+        <VideoListItem 
+            v-for="video in videos"
+            :video="video"
+            :key="video.etag"
+            @videoSelect="onVideoSelect"
+        >
+        </VideoListItem>
     </ul>
 </template>
 
@@ -12,8 +17,13 @@ export default {
     name: 'VideoList',
     components: {
         VideoListItem
+    },
+    props: ['videos'],
+    methods: {
+        onVideoSelect(video) {
+            this.$emit('videoSelect', video)
+        }
     }
-    props: ['videos']
 }
 </script>
 
